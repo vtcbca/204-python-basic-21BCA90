c1e7eb8ddfc4b93600c583f8197ac23b4528340b
Laxman sirvi <100264240+21BCA90@users.noreply.github.com>
2022-03-27 17:09:21 +0530

    solution 3
    Here I am uploading my journal program.

---
+++ Solution/Solution 3.py
@@ -0,0 +1,9 @@
+#WAS to enter and check it is even number or not.
+#Name : Laxman Sirvi
+#Date : 09-03-22
+n=int(input("Enter any No. : "))
+if n%2==0:
+        print("{} is even number".format(n))
+else:
+    print("{} is odd number".format(n))
+    
