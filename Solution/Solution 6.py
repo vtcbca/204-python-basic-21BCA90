222f271ae91a5518b3220938f484a31d0e461723
Laxman sirvi <100264240+21BCA90@users.noreply.github.com>
2022-03-27 17:10:53 +0530

    solution 6
    Here I am uploading my journal program.

---
+++ Solution/Solution 6.py
@@ -0,0 +1,21 @@
+#WAS to enter any number and print approprite days name
+#Name : Laxman Sirvi
+#Date : 09-03-22
+a=int(input("Enter any No. between 1 to 7 : "))
+if a==1:
+    print("Sunday")
+elif a==2:
+    print("Monday")
+elif a==3 :
+    print("Tuesday")
+elif a==4:
+    print("Wednesday")
+elif a==5:
+    print("Thursday")
+elif a==6:
+    print("Friday")
+elif a==7:
+    print("Saturday")
+else:
+    print("Plz... enter a valid No.")
+    
