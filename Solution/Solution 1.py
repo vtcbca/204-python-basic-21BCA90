94bb16766aad0ccbf713396239bd8f21dbce38e7
Laxman sirvi <100264240+21BCA90@users.noreply.github.com>
2022-03-27 17:08:10 +0530

    solution 1
    Here I am uploading my journal program.

---
+++ Solution/Solution 1.py
@@ -0,0 +1,6 @@
+#Write a python script to enter two No. & print the sum of it.
+#Name : Laxman Sirvi
+#Date : 09-03-22
+a=int(input("Enter value for a : "))
+b=int(input("Enter value for b : "))
+print("Sum of {} and {} = {}".format(a,b,(a+b)))
