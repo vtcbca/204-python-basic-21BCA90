d15c5c332bf1fb4ca4c3dc4fd47e916e0dfd080a
Laxman sirvi <100264240+21BCA90@users.noreply.github.com>
2022-03-27 17:11:29 +0530

    solution 8
    Here I am uploading my journal program.

---
+++ Solution/Solution 8.py
@@ -0,0 +1,8 @@
+#WAS to print the sum of  1 to n number
+#Name : Laxman Sirvi
+#Date : 09-03-22
+n=int(input("Enter any No. to print the sum of N No. : "))
+sum=0
+for a in range(n+1):
+    sum=sum+a
+print("The sum of N No. is ",sum)
