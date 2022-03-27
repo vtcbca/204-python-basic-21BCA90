24ccb1994692e86e949ef9c6b343aae99850a169
Laxman sirvi <100264240+21BCA90@users.noreply.github.com>
2022-03-27 17:08:51 +0530

    solution 2
    Here I am uploading my journal program.

---
+++ Solution/Solution 2.py
@@ -0,0 +1,5 @@
+#WAS to enter kilometer and converted it into meter.
+#Name : Laxman Sirvi
+#Date : 09-03-22
+km=float(input("Enter a value of kilometer : "))
+print("{} Km = {} meter".format(km,(km*1000)))
