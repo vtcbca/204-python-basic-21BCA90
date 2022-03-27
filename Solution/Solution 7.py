85bfbd93053fad2709e8cef31ba2d4609c2db37e
Laxman sirvi <100264240+21BCA90@users.noreply.github.com>
2022-03-27 17:11:12 +0530

    solution 7
    Here I am uploading my journal program.

---
+++ Solution/Solution 7.py
@@ -0,0 +1,9 @@
+#WAS to print number from 1 to n using while loop
+#Name : Laxman Sirvi
+#Date : 09-03-22
+n=int(input("Enter any No. to print series of N No. : "))
+i=1
+while i<n+1:
+    print(i)
+    i=i+1
+    
